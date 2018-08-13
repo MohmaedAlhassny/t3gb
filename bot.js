d53efdd62b53662ad6b3bfcbd09d46cb97881b77
Mohmaed Ayad <40663543+MohmaedAlhassny@users.noreply.github.com>
2018-08-13 20:02:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('ready', () => {
     console.log(`in ${client.guilds.size} servers `)
     console.log(`[Alhassny Orders] ${client.users.size}`)
     client.user.setStatus("DND");
-    client.user.setActivity('Alhassny Orders.',{type: 'PLAYING'});
+    client.user.setActivity('Dynasty Server.',{type: 'LISTENING'});
 });
 //by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
 const prefix = "!";
